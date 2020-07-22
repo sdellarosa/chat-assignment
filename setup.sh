0b57c428d981f497e7ac410f3c7da5fe6ceeddb1
Sonnya Dellarosa <dion_sonnya@yahoo.com>
2020-07-22 17:28:39 +0200

    Modified setup file

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 composer install
 cp .env.example .env
 php artisan key:generate
-sed -i -e 's|DB_DATABASE=|DB_DATABASE=$PWD/database/database.sqlite|g' .env
+sed -i -e 's|DB_DATABASE=|DB_DATABASE='`pwd`'/database/database.sqlite|g' .env
 echo > database/database.sqlite
 php artisan migrate
 php artisan db:seed
